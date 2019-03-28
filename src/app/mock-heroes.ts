92a6f9308ef9cf5f83c9c31ecc33bdbeebebe62f
Chase Derr <chase.derr@treehousei.net>
2019-03-27 18:45:00 -0600

    working heroes tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,18 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 1, name: 'Mr. Nice', gender:'Male' },
+  { id: 2, name: 'Narco', gender:'Male' },
+  { id: 3, name: 'Black Widow', gender:'Female' },
+  { id: 4, name: 'Bombasto', gender:'Male' },
+  { id: 5, name: 'Celeritas', gender:'Male' },
+  { id: 6, name: 'Magneta', gender:'Male' },
+  { id: 7, name: 'Wonder Woman', gender:'Female' },
+  { id: 8, name: 'RubberMan', gender:'Male' },
+  { id: 9, name: 'Dynama', gender:'Male' },
+  { id: 10, name: 'Dr IQ', gender:'Male' },
+  { id: 11, name: 'Violet Incredible', gender:'Female' },
+  { id: 12, name: 'Magma', gender:'Male' },
+  { id: 13, name: 'Tornado', gender:'Male' },
+  { id: 14, name: 'Elastagirl', gender:'Female' }
+];
